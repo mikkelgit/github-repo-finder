3f7a17f1645037057a8fbd71dd69517e4138e106
Mike <Mike@Legion>
2021-06-15 16:42:37 +0700

    fetching data from github api done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import illustration from './assets/illustration.svg'
 
 export default function App() {
+    const [repositoryData, setRepositoryData] = useState([])
+    const [inputData, setInputData] = useState('')
 
-    const findRepos = (e) => {
+    const findRepos = async (e) => {
         e.preventDefault()
-        console.log('function here');
+        if (inputData.length) {
+            const response = await (await fetch('https://api.github.com/users/' + inputData + '/repos')).json()
+            console.log(response)
+        }
     }
 
     return (
@@ -13,7 +19,7 @@ export default function App() {
                 <p className="text-lg text-center md:text-2xl font-bold">Find GitHub Repository by Username</p>
                 <form onSubmit={(e) => findRepos(e)} >
                     <div className="bg-white flex rounded-lg px-4 py-2 max-w-screen-sm mx-auto" >
-                        <input className="bg-transparent focus:outline-none w-full" type="text" placeholder="username goes here" />
+                        <input value={inputData} onChange={(e) => setInputData(e.target.value)} className="bg-transparent focus:outline-none w-full" type="text" placeholder="username goes here" />
                         <button>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -21,6 +27,9 @@ export default function App() {
                         </button>
                     </div>
                 </form>
+                <div className="pt-16">
+                    <img className="block md:max-w-screen-sm opacity-50 mx-auto" src={illustration} alt="" />
+                </div>
             </div>
         </div>
     )
